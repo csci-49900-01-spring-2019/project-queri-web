e65075966bce27e7faf78e05e120557adac33d9b
Ramela Ramnauth <ramelaramnauth@outlook.com>
2019-03-14 14:38:24 -0400

    uploaded angular project

---
+++ test_angular/Queri/Queri/src/app/app.module.ts
@@ -0,0 +1,44 @@
+
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopicListComponent } from './topic-list/topic-list.component';
+import { TopicComponent } from './topic/topic.component';
+import { AskComponent } from './ask/ask.component';
+import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopicListComponent,
+    TopicComponent,
+    AskComponent,
+    HomeComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    //NgbModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path:'Ask', 
+        component: AskComponent,
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  
+})
+export class AppModule { }
