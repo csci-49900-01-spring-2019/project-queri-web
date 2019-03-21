f6efd797886161067e125573ba617b1b13a7b4a2
Ramela Ramnauth <ramelaramnauth@outlook.com>
2019-03-21 16:35:03 -0400

    added project

---
+++ Angular/Queri/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {DemoService} from './demo.service';
+import {Observable} from 'rxjs';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+ 
+  public foods;
+ 
+  constructor(private _demoService: DemoService) { 
+
+    _demoService.getFoods();
+  }
+  
+  
+  
+}
---
+++ Angular/Queri/src/app/topic/topic.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {NgModule, Input}      from '@angular/core';
+
+export class Topic{
+  public name: string;
+
+  constructor(type: string){
+    this.name = type;
+  }
+}
+
+@Component({
+  selector: 'topic', //was app-topic
+  templateUrl: './topic.component.html',
+  styleUrls: ['./topic.component.css']
+})
+export class TopicComponent implements OnInit {
+
+  @Input('topic') data: Topic;
+  constructor() {}
+  ngOnInit() {
+  }
+}
