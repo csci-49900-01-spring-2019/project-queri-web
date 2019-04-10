87717fdca4f1f72210f7a2ea1e6c2ea45bfc978b
rramnauth11 <ramelaramnauth@outlook.com>
2019-04-10 10:18:15 -0400

    removed topics component from html

--- Queri/src/app/ask/ask.component.ts
+++ Queri/src/app/ask/ask.component.ts
@@ -1,10 +1,12 @@
 
 import { Component, OnInit } from '@angular/core';
+import { DemoService } from '../demo.service';
 
 @Component({
   selector: 'app-ask',
   templateUrl: './ask.component.html',
-  styleUrls: ['./ask.component.css']
+  styleUrls: ['./ask.component.css'],
+  providers: []
 })
 export class AskComponent implements OnInit {
 
