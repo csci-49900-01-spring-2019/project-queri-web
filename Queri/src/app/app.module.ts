e578c56677af1d6a0d3984c1835126445b9e05e8
rramnauth11 <ramelaramnauth@outlook.com>
2019-04-13 12:56:02 -0400

    adding styling

--- Queri/src/app/app.module.ts
+++ Queri/src/app/app.module.ts
@@ -9,13 +9,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { DemoService } from './demo.service';
-//import { TopicListComponent } from './shared/topic-list/topic-list.component';
-//import { TopicComponent } from './models/topic-list/topic/topic.component';
 import { AskComponent } from './ask/ask.component';
 import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { QuestionComponent } from './home/question/question.component';
-//import { ObjectToArrayPipe } from './pipes/object-to-array.pipe';
 import { CommentComponent } from './home/comment/comment.component';
 import { AskFormComponent } from './ask/ask-form/ask-form.component';
 import { CommentFormComponent } from './home/comment-form/comment-form.component';
@@ -25,14 +22,10 @@ import { CommentFormComponent } from './home/comment-form/comment-form.component
 @NgModule({
   declarations: [
     AppComponent,
-    //TopicListComponent,
-    //TopicComponent,
     AskComponent,
     HomeComponent,
     QuestionComponent,
-    //ObjectToArrayPipe,
     CommentComponent,
-    //FormsModule,
     AskFormComponent,
     CommentFormComponent
 
@@ -48,13 +41,12 @@ import { CommentFormComponent } from './home/comment-form/comment-form.component
         path: '', component: HomeComponent
       },
       {
-        path:'Ask', 
+        path: 'Ask',
         component: AskComponent,
       }
     ])
   ],
   providers: [DemoService, HttpClientModule],
   bootstrap: [AppComponent],
-  
 })
 export class AppModule { }
