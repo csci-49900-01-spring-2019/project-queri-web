9423bb18e5c1410b240e2d2023c6c689e2530818
rramnauth11 <ramelaramnauth@outlook.com>
2019-05-02 14:33:23 -0400

    fixed merge conflict

--- Queri/src/app/_models/data.ts
+++ Queri/src/app/_models/data.ts
@@ -16,12 +16,3 @@ interface comment {
     content: string;
     username: string;
 }
-<<<<<<< HEAD
-
-interface statistics {
-    comments: number;
-    days_remaining: number;
-    likes: number;
-}
-=======
->>>>>>> 8dff863a91d487801fa63d224be3fd590761b739
