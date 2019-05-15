d47684b9097f1921d864af4de0c5698c4a7d3b94
seorea <kevinyang5694@gmail.com>
2019-05-15 13:39:01 -0400

    Merge conflict with build working

--- Queri/src/app/home/home.component.ts
+++ Queri/src/app/home/home.component.ts
@@ -51,48 +51,29 @@ export class HomeComponent implements OnInit {
   result: boolean;
 
   async ngOnInit() {
-   // if (localStorage.getItem('') === '' || localStorage.getItem('') == null) {
-  //} else {
-    this.param = this.route.snapshot.params['id'];
-    this.currentPostNumber = 0;
-    console.log('Param: ', this.param);
-    console.log(this.posts);
-    this.posts = await this.getData();
+      this.param = this.route.snapshot.params['id'];
+      this.currentPostNumber = 0;
+      console.log('Param: ', this.param);
+      console.log(this.posts);
+      this.posts = await this.getData();
 
-    console.log(this.posts);
-    console.log(this.posts.length);
-    this.postsKeys = Object.keys(this.posts);
-    console.log(this.postsKeys);
-    this.title.setTitle('Home');
-    this.result = this.validateParam();
-    console.log(this.result);
-    if ( this.result === false) {
-      this.router.navigateByUrl('/404');
-    } else {
-      // console.log(this.currentPostNumber);
-      this.currentPostNumber = +this.param;
-      // console.log(this.currentPostNumber);
-      this.setRandomColor();
+      console.log(this.posts);
+      console.log(this.posts.length);
+      this.postsKeys = Object.keys(this.posts);
+      console.log(this.postsKeys);
+      this.title.setTitle('Home');
+      this.result = this.validateParam();
+      console.log(this.result);
+      if ( this.result === false) {
+        this.router.navigateByUrl('/404');
+      } else {
+        // console.log(this.currentPostNumber);
+        this.currentPostNumber = +this.param;
+        // console.log(this.currentPostNumber);
+        this.setRandomColor();
+      }
     }
-  //}
-}
-
-
-async getData() {
-  let temp: Post[];
-  await this.demoService.getAll(this.type)
-  .toPromise()
-  .then( post => {
-    console.log(post);
-    temp = post; }
-    /*post => {
-    this.posts = post;
-    this.postsKeys = Object.keys(post);
-    console.log('Promise Resolved');*/
-  );
- // console.log(temp);
-  return temp;
-}
+  
 
   /*
   async getData() {
@@ -121,64 +102,59 @@ async getData() {
     return temp;
   }
 
-  // For Rotating Colors
-  this.color = this.colors[this.currentPostNumber % this.colors.length];
-}
+  setRandomColor(): void {
+    // For Random Colors
+    // const index = Math.floor(Math.random() * this.numberOfColors) + 0;
+    // this.color = this.colors[index];
 
-onClickPrevious() {
-  this.showComments = false;
-  if ( +this.currentPostNumber === 0) {
-    this.currentPostNumber = +this.postsKeys.length - 1;
-  } else {
-    // console.log('else');
-    this.currentPostNumber = +this.currentPostNumber - 1;
+    // For Rotating Colors
+    this.color = this.colors[this.currentPostNumber % this.colors.length];
   }
-  console.log(this.currentPostNumber);
-  this.setRandomColor();
-  this.router.navigate(['/featured', this.postsKeys[this.currentPostNumber]]);
-}
 
-onClickNext() {
-  this.showComments = false;
-  if (this.currentPostNumber === this.postsKeys.length - 1) {
-    this.currentPostNumber = 0;
-  } else {
-    this.currentPostNumber = +this.currentPostNumber + 1;
-  };
-  // console.log(this.currentPostNumber);
-  this.setRandomColor();
-  this.router.navigate(['/featured', this.postsKeys[this.currentPostNumber]]);
-}
-
-onClickAnswer() {
-  this.showForm = !this.showForm;
-}
-
-async setDataFromchild( data ) {
-  await this.demoService.getCommentsInPostInView(this.type, this.postsKeys[this.currentPostNumber])
-  .subscribe(comments => {
-    // console.log('Comments: ', comments);
-    this.comments = comments;
-    this.commentkeys = Object.keys(this.comments);
-    // console.log('Comments: ', this.comments);
-    // console.log('# of Comments: ', this.commentkeys.length);
-  });
-  this.showForm = false;
-  this.showComments = true;
-}
-
-onTest() {
-  this.router.navigateByUrl('/page_not_found');
-}
-
-validateParam(): boolean{
-  for( const i of this.postsKeys ) {
-    if (i === this.param) {
-      console.log('match!');
-      return true;
+  onClickPrevious() {
+    this.showComments = false;
+    if ( +this.currentPostNumber === 0) {
+      this.currentPostNumber = +this.postsKeys.length - 1;
     } else {
-      console.log('not a match');
+      // console.log('else');
+      this.currentPostNumber = +this.currentPostNumber - 1;
     }
+    console.log(this.currentPostNumber);
+    this.setRandomColor();
+    this.router.navigate(['/featured', this.postsKeys[this.currentPostNumber]]);
+  }
+
+  onClickNext() {
+    this.showComments = false;
+    if (this.currentPostNumber === this.postsKeys.length - 1) {
+      this.currentPostNumber = 0;
+    } else {
+      this.currentPostNumber = +this.currentPostNumber + 1;
+    };
+    // console.log(this.currentPostNumber);
+    this.setRandomColor();
+    this.router.navigate(['/featured', this.postsKeys[this.currentPostNumber]]);
+  }
+
+  onClickAnswer() {
+    this.showForm = !this.showForm;
+  }
+
+  async setDataFromchild( data ) {
+    await this.demoService.getCommentsInPostInView(this.type, this.postsKeys[this.currentPostNumber])
+    .subscribe(comments => {
+      // console.log('Comments: ', comments);
+      this.comments = comments;
+      this.commentkeys = Object.keys(this.comments);
+      // console.log('Comments: ', this.comments);
+      // console.log('# of Comments: ', this.commentkeys.length);
+    });
+    this.showForm = false;
+    this.showComments = true;
+  }
+
+  onTest() {
+    this.router.navigateByUrl('/page_not_found');
   }
 
   validateParam(): boolean{
@@ -193,4 +169,3 @@ validateParam(): boolean{
     return false;
   }
 }
-  }
